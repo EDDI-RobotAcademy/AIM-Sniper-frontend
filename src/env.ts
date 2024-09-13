7c87938b10bc56ec4d7f84af1840b11edd4b9fcd
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-09-13 14:32:38 +0900

    feat: [최민지] 홈 소개 페이지 추가 [AIMSFV-4]

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+interface Env {
+    mode: string | undefined
+    api: {
+        MAIN_API_URL: string | undefined
+        AI_BASE_URL: string | undefined
+    }
+}
+
+const env: Env = {
+    mode: process.env.MODE_ENV,
+    api: {
+        MAIN_API_URL: process.env.VUE_APP_BASE_URL,
+        AI_BASE_URL: process.env.VUE_APP_AI_BASE_URL,
+    },
+}
+
+export default env
---
+++ src/home/pages/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  홈페이지
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomeView',
+
+  data () {
+    return {
+      data: ''
+    }
+
+  },
+  methods: {
+    defaultFunc () {
+      console.log('홈입니다')      
+    }
+  }
+};
+</script>
