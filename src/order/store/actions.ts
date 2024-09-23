30fe4d785dd20588f4810ad9ba5d978f5aab5ea9
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-09-23 10:37:55 +0900

    feat: [김용현] 사용자가 보고서 구매를 요청합니다. [AIMSFV-28]

---
+++ src/order/store/actions.ts
@@ -0,0 +1,73 @@
+import { ActionContext } from "vuex"
+import { Order, OrderItem, OrderState } from "./states"
+import { AxiosResponse } from "axios"
+import axiosInst from "@/utility/axiosInstance"
+
+export type OrderActions = {
+    requestCartToAddOrderToDjango(
+        context: ActionContext<OrderState, any>,
+        payload: {
+            userToken: string;
+            items: {
+                cartItemId: number;
+                quantity: number;
+                orderPrice: number
+            }[]
+        }
+    ): Promise<AxiosResponse>;
+    requestProductReadToAddOrderToDjango(
+        context: ActionContext<OrderState, any>,
+        payload: {
+            userToken: string,
+            productId: number,
+            productPrice: number,
+        }
+}
+
+const actions: OrderActions = {
+    async requestCartToAddOrderToDjango({ state }, payload) {
+        try {
+            const userToken = sessionStorage.getItem('userToken')
+            if (!userToken) {
+                throw new Error('User token not found')
+            }
+            
+            console.log('payload:', payload)
+            const requestData = { 
+                userToken,
+                items: payload.items.map(item => ({
+                    cartItemId: item.cartItemId,
+                    quantity: item.quantity,
+                    orderPrice: item.orderPrice
+                }))
+            }
+
+            const response = await axiosInst.djangoAxiosInst.post('/orders/cart', requestData)
+            console.log('response data:', response.data)
+            
+            return response.data
+        } catch (error) { 
+            console.error('장바구니 페이지에서 상품 구매시 에러 발생:', error)
+            throw error
+        }
+    },
+    async requestProductReadToAddOrderToDjango(context: ActionContext<OrderState, any>, payload: {
+        userToken: string, productId: number, productPrice: number}
+        ): Promise<void> {
+        try {
+            const { userToken, productId, productPrice } = payload
+            
+            console.log('payload:', payload)
+
+            const response = await axiosInst.djangoAxiosInst.post('/orders/product', payload )
+            console.log('response data:', response.data)
+            
+            return response.data
+        } catch (error) { 
+            console.error('상품 페이지에서 상품 구매 시 에러 발생:', error)
+            throw error
+        }
+    },
+}
+
+export default actions
