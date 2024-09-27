99e77f005927714a1043caaa39b9b6fc259ab534
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-09-27 16:43:48 +0900

    add: [최민지] 설문조사에 참여한 사용자일 경우 설문 참여 제한 기능 추가 [AIMSFV-15]

--- src/survey/store/actions.ts
+++ src/survey/store/actions.ts
@@ -2,7 +2,7 @@ import { ActionContext } from "vuex"
 import { Survey, SurveyState } from "./states"
 import { AxiosResponse } from "axios"
 import axiosInst from "@/utility/axiosInstance"
-import { REQUEST_SURVEY_LIST_TO_DJANGO, REQUEST_SURVEY_FORM_TO_DJANGO } from "./mutation-types"
+import { REQUEST_SURVEY_LIST_TO_DJANGO, REQUEST_SURVEY_FORM_TO_DJANGO, REQUEST_RESULT_FORM_TO_DJANGO } from "./mutation-types"
 
 export type SurveyActions = {
     requestCreateSurveyFormToDjango(context: ActionContext<any, any>, randomString: string): Promise<AxiosResponse>
@@ -18,6 +18,8 @@ export type SurveyActions = {
     requestSurveyFormToDjango(context: ActionContext<SurveyState, any>, randomString: string): Promise<void>
     requestSubmitSurveyToDjango(context: ActionContext<any, any>, payload: {submitForm: []}): Promise<AxiosResponse>
     requestRandomStringToDjango(context: ActionContext<SurveyState,any>,px: number): Promise<any>
+    requestSurveyResultToDjango(context: ActionContext<SurveyState, any>, surveyId: number): Promise<void>
+    requestCheckIsFirstSubmit(context: ActionContext<any, any>, accountId: {accountId : string}): Promise<AxiosResponse>
 }
 
 const actions: SurveyActions = {
@@ -68,7 +70,16 @@ const actions: SurveyActions = {
           console.error("Django 서버에서 랜덤 문자열을 받아오지 못했습니다:", error);
           return null;
         }
-    }      
+    },
+    async requestSurveyResultToDjango(context: ActionContext<SurveyState, any>, surveyId: number): Promise<void> {
+        const res: AxiosResponse<Survey> = await axiosInst.djangoAxiosInst.get(`/survey/survey-result/${surveyId}`);
+        context.commit(REQUEST_RESULT_FORM_TO_DJANGO, res.data);
+    },
+    async requestCheckIsFirstSubmit(context: ActionContext<any, any>, accountId: {accountId : string}): Promise<AxiosResponse> {
+        const res: AxiosResponse = await axiosInst.djangoAxiosInst.post('/survey/check-first-submit', accountId)
+        return res.data 
+    }
+
 };
 
 export default actions;
