5385f6870fde995143dfcc32f85ca7b62acdd53b
y0ng98 <dldydgnl2080@gmail.com>
2024-10-18 19:08:53 +0900

    feat: [이용휘] 다음 면접 질문 생성 요청 [AIMSFV-70]

--- src/aiInterview/store/actions.ts
+++ src/aiInterview/store/actions.ts
@@ -4,12 +4,33 @@ import axiosInst from "@/utility/axiosInstance"
 
 export type AIInterviewActions = {
     requestGetQuestionListToDjango(context: ActionContext<any, any>, sessionId: number): Promise<AxiosResponse>
-}
+    requestInferNextQuestionToFastAPI(
+        context: ActionContext<any, any>,
+        payload: { answer: string, nextIntent: string }): Promise<string>
+}   
 
 const actions: AIInterviewActions = {
     async requestGetQuestionListToDjango(context: ActionContext<any, any>, sessionId: number): Promise<AxiosResponse> {
         const res: AxiosResponse = await axiosInst.djangoAxiosInst.post('/interview/get-session', sessionId)
         return res.data
+    },
+    async requestInferNextQuestionToFastAPI(
+        context: ActionContext<any, any>,
+        payload: { answer: string, nextIntent: string }): Promise<string> {
+        console.log("payload:", payload)
+        const { answer, nextIntent } = payload
+        try {
+            console.log('requestInferNextQuestionToFastAPI()')
+            console.log("userInput:", answer)
+            const command = 7
+
+            const response = await axiosInst.fastapiAxiosInst.post(
+                '/request-ai-command', { command, "data":[answer, nextIntent] })
+            return response.data
+        } catch (error) {
+            console.log('requestInferToFastAPI() 중 문제 발생:', error)
+            throw error
+        }
     },
 }
 
