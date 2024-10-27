9dbdf2fed1b5352307b41010eb5614b553d0fe0a
Kyeongmin <124436145+2kilometer@users.noreply.github.com>
2024-10-28 07:46:23 +0900

    feat: [이경민] 로그 데이터 수집을 위해 GA 도입 [AIMSFV-61]

---
+++ nuxt-app/plugins/vgtag.js
@@ -0,0 +1,15 @@
+// plugins/vgtag.js
+import { defineNuxtPlugin } from '#app'
+import VueGtag from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const isDev = process.env.NODE_ENV !== 'production'; // 개발 환경 체크
+
+  if (!isDev) {
+    nuxtApp.vueApp.use(VueGtag, {
+      property: {
+        id: process.env.GA_TRACKING_ID, // GA 측정 ID
+      },
+    });
+  }
+});
