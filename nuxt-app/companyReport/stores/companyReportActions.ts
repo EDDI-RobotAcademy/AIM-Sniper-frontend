ad6a85d0037332baa5bf8ab1f3e44261f5efbc6a
Kyeongmin <124436145+2kilometer@users.noreply.github.com>
2024-10-27 18:56:25 +0900

    feat: [이경민] companyReportReadPage의 nuxt 전환의 초기구성 [AIMSFV-88]

--- nuxt-app/companyReport/stores/companyReportActions.ts
+++ nuxt-app/companyReport/stores/companyReportActions.ts
@@ -30,7 +30,80 @@ export const companyReportActions = {
 		}
 	},
 
+	async requestCreateCompanyReportToDjango(imageFormData: FormData): Promise<void> {
+		const { djangoAxiosInst } = axiosUtility.createAxiosInstances();
 
+		try {
+			const res = await djangoAxiosInst.post('/company_report/register', imageFormData, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			});
+			alert('보고서를 성공적으로 등록하셨습니다.');
+			return res;
+		} catch (error) {
+			console.log('requestCreateCompanyReportToDjango() -> error:', error);
+			throw error;
+		}
+	},
+
+	async requestDeleteCompanyReportToDjango(companyReportId: number): Promise<void> {
+		const { djangoAxiosInst } = axiosUtility.createAxiosInstances();
+		
+		try {
+			await djangoAxiosInst.delete(`/company_report/delete/${companyReportId}`);
+		} catch (error) {
+			console.log('requestDeleteCompanyReportToDjango() -> error:', error);
+			throw error;
+		}
+	},
+	
+    async requestModifyCompanyReportToDjango(payload: {
+        companyReportName: string;
+		content: string;
+		companyReportId: number;
+		companyReportPrice: number;
+		keyword:string
+    }): Promise<void> {
+        const { companyReportName, content, companyReportId,companyReportPrice,keyword } = payload
+		const { djangoAxiosInst } = axiosUtility.createAxiosInstances();
+
+        try {
+            await djangoAxiosInst.put(`/company_report/modify/${companyReportId}`, {
+				companyReportName, 
+				content, 
+				companyReportPrice,
+				keyword 
+			});
+        } catch (error) {
+            console.log('requestModifyCompanyReportToDjango() 과정에서 문제 발생')
+            throw error
+        }
+    },
+
+	async requestCompanyReportFinanceToDjango(companyReportName: string): Promise<any> {
+		const { djangoAxiosInst } = axiosUtility.createAxiosInstances();
+
+		try {
+			const data = await djangoAxiosInst.post('company_report/finance', { companyReportName });
+			return data;
+		} catch (error) {
+			console.log('requestCompanyReportFinanceToDjango() -> error:', error);
+			throw error;
+		}
+	},
+
+	async requestCompanyReportInfoToDjango(companyReportName: string): Promise<any> {
+		const { djangoAxiosInst } = axiosUtility.createAxiosInstances();
+		
+		try {
+			return await djangoAxiosInst.post('company_report/info', { companyReportName });
+		} catch (error) {
+			console.log('requestCompanyReportInfoToDjango() -> error:', error);
+			throw error;
+		}
+	},
+	
 	async requestTopNCompanyReportListToDjango(topN: number): Promise<void> {
 		const { djangoAxiosInst } = axiosUtility.createAxiosInstances();
 		
