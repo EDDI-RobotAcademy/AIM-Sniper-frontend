f82d145a93b91b12f1364d0b6141d19e9b8ad37f
Kyeongmin <124436145+2kilometer@users.noreply.github.com>
2024-10-31 14:21:54 +0900

    fix: [이경민] 변수명 수정 [AIMSFV-61]

--- nuxt-app/plugins/vgtag.js
+++ nuxt-app/plugins/vgtag.js
@@ -1,14 +1,16 @@
 // plugins/vgtag.js
 import { defineNuxtPlugin } from '#app'
+import { useRuntimeConfig } from 'nuxt/app';
 import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const isDev = process.env.NODE_ENV !== 'production'; // 개발 환경 체크
+  const runtimeConfig = useRuntimeConfig();
 
   if (!isDev) {
     nuxtApp.vueApp.use(VueGtag, {
       property: {
-        id: process.env.GA_TRACKING_ID, 
+        id: runtimeConfig.public.GA_MEASUREMENT_ID,
       },
     });
   }
